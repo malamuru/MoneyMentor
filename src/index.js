cf7f2bf60859e9f06e468e85441b808e171efe5c
Mounika Alamuru <malamuru@uncc.edu>
2026-01-20 21:42:54 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <ToastContainer />
-      <App />
-    </HashRouter>
+    <ToastContainer />
+    <App />
   </React.StrictMode>
 );
